9880fea2dcc46c2e08ddd9a12683046ae383af40
mi82mi82 <daimi.0603@icolud.com>
2023-05-26 12:21:03 +0900

    バリデーション　use宣言変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\controller\Auth\Validator;
 use Auth;
 
 class LoginController extends Controller
