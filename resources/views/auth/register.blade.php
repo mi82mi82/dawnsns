c77dc5e3355e04fe430234f7c2ea373c588e56d8
mi82mi82 <daimi.0603@icolud.com>
2023-05-26 15:01:16 +0900

    バリデーションのエラーメッセージがあるかどうか

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,5 +24,14 @@
 
 {!! Form::close() !!}
 
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
 @endsection
