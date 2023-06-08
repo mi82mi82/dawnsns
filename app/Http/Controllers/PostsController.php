4d3f73f36b19c84408f8ea69c8060a05480acfdc
mi82mi82 <daimi.0603@icolud.com>
2023-06-08 17:49:45 +0900

    $user = auth()->user();  　はmyadminのuserテーブルからuserの情報をとってきている $posts = DB::〜のところはmyadminのpostsテーブルからpostsの情報をとってきている　のと、 ->join で　userテーブルとpostsテーブルを結合して、usersの各テーブルの同じidをとるように記述してる　のと、 ->select で　同じidとる時にidの情報全部ではなく、ほしい情報だけ記述している(例 users.images :usersテーブルのプロフィール画像をとってきてる、posts.posts : postsテーブルのposts(投稿内容)とってきている、posts.created_at as created_at　: posts テーブルから投稿時間取得してる＜ as created_at ＞入れなきゃいけない) ->where 〜のところはusers.idを　Auth::id()  は現在認証しているユーザーのIDを取得している
    ->get(); はgetでとってくるようにしている
    とってきたのを、
    view( 'posts.index',  compact('user','posts') );
    　postsのindex.blade.phpからとってる、　compact　はviewの変数とcontrollerで定義した変数の名前が同じ時にcompact関数が使える

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
     //
     public function index(){
         $user = auth()->user();
-        return view('posts.index',compact('user'));
+        $posts = DB::table('posts')
+        ->join('users','posts.user_id','=','users.id')
+        ->where('users.id',Auth::id())
+        ->select('users.images','users.username','posts.id','posts.posts','posts.created_at as created_at')
+        ->get();
+        return view('posts.index',compact('user','posts'));
     }
 
     // ログインユーザーのみが使えるようにする
