e5ff009c2eed1ea08e888affa6d9dd4956692ede
mi82mi82 <daimi.0603@icolud.com>
2023-06-09 17:14:15 +0900

    投稿内容の更新　ルーティング記述

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::get('/added', 'Auth\RegisterController@added');
 //ログイン中のページ
 Route::get('/top','PostsController@index');
 Route::get('post/create-form', 'PostsController@createForm');
+Route::get('post/{id}/update-form', 'PostsController@updateForm');
 Route::get('/profile','UsersController@profile');
 
 Route::get('/search','UsersController@index');
