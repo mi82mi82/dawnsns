83b19c8b13efc1919b30dae84b291024dbdb9599
mi82mi82 <daimi.0603@icolud.com>
2023-06-29 18:24:54 +0900

    snsトップロゴ、プロフィール画像追加

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -22,10 +22,10 @@
 <body>
     <header>
         <div id = "head">
-        <h1><a><img src="images/logo.png"></a></h1>
+        <h1><a><img src="images/main_logo.png"></a></h1>
             <div id="">
                 <div id="">
-                    <p>{{ $user->username }}<img src="images/arrow.png"></p>
+                    <p>{{ $user->username }}<img src="images/dawn.png"></p>
                 <div>
                 <ul>
                     <li><a href="/top">ホーム</a></li>
