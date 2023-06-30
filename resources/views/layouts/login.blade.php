e1b2680b5a21b036dfef0fce732a2bac03b9895b
mi82mi82 <daimi.0603@icolud.com>
2023-06-30 16:43:00 +0900

    Revert "Revert "Revert "プルダウンメニュー作ろうとしたけど、できんかった！！"""
    This reverts commit 73fe544dfcba987f92fd35886ee729049044f937.

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -21,20 +21,19 @@
 </head>
 <body>
     <header>
-     <nav class="g-navi active">
-        <div class = "container nav-wrapper">
-               <h1><a><img src="images/main_logo.png"></a></h1>
-                        <div class="menu-trigger">
-                            <p>{{ $user->username }}<img src="images/dawn.png"></p>
-                        <div>
-                          <ul>
-                              <li><a href="/top">ホーム</a></li>
-                              <li><a href="/profile">プロフィール</a></li>
-                              <li><a href="/logout">ログアウト</a></li>
-                            </ul>
-                    </div>
-             </div>
-            </nav>
+        <div id = "head">
+        <h1><a><img src="images/main_logo.png"></a></h1>
+            <div id="">
+                <div id="">
+                    <p>{{ $user->username }}<img src="images/dawn.png"></p>
+                <div>
+                <ul>
+                    <li><a href="/top">ホーム</a></li>
+                    <li><a href="/profile">プロフィール</a></li>
+                    <li><a href="/logout">ログアウト</a></li>
+                </ul>
+            </div>
+        </div>
     </header>
     <div id="row">
         <div id="container">
