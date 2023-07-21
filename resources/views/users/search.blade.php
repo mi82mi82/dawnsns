ec5f8641cd4d1b33d5085d79cd47ac3092534154
mi82mi82 <daimi.0603@icolud.com>
2023-07-21 14:30:11 +0900

    12行目、＠foreach()と＠endforeach記述し、中に繰り返し処理したい配列記述。 $getUser配列の要素1つずつがforeach内でのみ使える変数$getUserとして使えるようになる 横配列になっちゃうから<div> </idv>タグで囲う ユーザーのプロフィール画像を表示したい(userControllerで記述した$getUsersから持ってくる) ユーザー名も表示したい　{{$getUser->username}}で表示
    foreach: 配列に含まれる要素を繰り返し処理で一つ一つ取得したり、逆に配列の値を変数に格納したりする際に使用する構文です。
    
    4行目は検索フォーム作成　(検索フォームはindex.blade.phpと記述ほとんど変わらないので、コピーして、ところでころ修正する)
    飛ぶ先(url)は/seach、
    classは　’　　’
    で
     <div class="">記述今はしない。
    
    {!! Form::input( の中はnewpostをsearchに修正、piaceholderは検索名内にユーザー名を記述したいので記入。
    <img>タグは不要なので、検索と記入

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.login')
 
 @section('content')
+{!! Form::open(['class' => ''],['url' => '/search']) !!}
+        <div class="">
+            {!! Form::input('text', 'search', null, ['required', 'class' => 'form-control', 'placeholder' => 'ユーザー名']) !!}
+						<button type="submit" class="btn btn-success pull-right">検索</button>
+        </div>
+        {!! Form::close() !!}
 
 
+@foreach($getUsers as $getUser)
+<div>
+<img src="images/{{$getUser->images}}">
+{{$getUser->username}}
+</div>
+@endforeach
 
 @endsection
