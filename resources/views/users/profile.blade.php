a0a9d18f3dfb09967cb78681ebba2e2fb7378c8c
mi82mi82 <daimi.0603@icolud.com>
2023-08-18 15:51:44 +0900

    Passwordの入力フォームの入力を不可にする為にreadonly属性を加える

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -19,10 +19,10 @@
         </div>
         <div class="ct-block">
 				<label class="contact-text" for="email">Password</label>
-          <input type="text" name="mail" placeholder="" class="form-mail">
+          <input type="text" name="Password" placeholder="" class="form-Password" value="readonly属性で入力規制" readonly>
         </div>
-				<label class="contact-text" for="email">new Password</label>
-          <input type="text" name="mail" placeholder="" class="form-mail">
+				<label class="contact-text" for="new Password">new Password</label>
+          <input type="text" name="new Password" placeholder="" class="form-new Password">
         </div>
         <div class="ct-block">
           <label for="request-about" class="contact-text">Bio</label>
