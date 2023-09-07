74c80886fb98fa91bb7083c9612d0c973f0304c4
mi82mi82 <daimi.0603@icolud.com>
2023-09-07 14:11:15 +0900

    usesProfile.blade.phpを作成。フォローリストやフォロワーリストのページのユーザーアイコンを押したら、ユーザーのプロフィールに飛ぶようにするため。 飛んだページはグレーの横線の下がfollowList.blade.phpやfollower.blade.phpと変わらないので、<table class= ~>コピペする。コピペした後<img src="/images/{{ $timeline->user->images }}">の<a>タグは不要なので削除。

---
+++ resources/views/users/usersProfile.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.login')
+
+@section('content')
+
+<table class='table table-hover'>
+		 <tr>
+				<th>投稿者アイコン</th>
+				<th>投稿者名</th>
+				<th>投稿内容</th>
+				<th>投稿日時</th>
+				<th></th>
+		 </tr>
+		 @foreach ($timelines as $timeline)
+			<tr class="posts-list">
+					<td class="posts-name">
+						<img src="/images/{{ $timeline->user->images }}">
+					</td>
+					<td class="posts-name">
+						<p>{{ $timeline->user->username }}"</p>
+					</td>
+					<td class="posts-post">
+						{{ $timeline->posts }}
+					</td>
+					<td class="posts-created_at">
+						{{ $timeline->created_at }}
+					</td>
+				</tr>
+		 @endforeach
+ </table>
+
+@endsection
