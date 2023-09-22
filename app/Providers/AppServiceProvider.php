bc0660f3e83d7e22f3d4e7fb30bbbf21e9f72006
mi82mi82 <daimi.0603@icolud.com>
2023-09-22 22:32:58 +0900

    フォロー数とフォロワー数のカウントを表示したい！！ AppServiceProviderのboot()メソッド内で、ビューコンポーザ　「 view()->composer 」を使用します。 28-31行目・・・ ビューコンポーザ　＝　ビュー全体にデータを共有するための変数。 　*はviewの全部、　$view・・・渡す変数を渡しための関数 29行目・・・データを所得するときにwith関数を使うfollowのカウント数、データベースのfollowsテーブルからwhereで条件をつける「 followerカラムにあるAuth::id( )でログインしているユーザー 」。 「」がフォローする　＝　followerになる( 私aaaaaaaaaaa1と一致する)
    ->count でカウントする

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('*', function($view){
+            $view->with('followCount', DB::table('follows')->where('follower', Auth::id())->count());
+            $view->with('followerCount', DB::table('follows')->where('follow', Auth::id())->count());
+        });
     }
 }
