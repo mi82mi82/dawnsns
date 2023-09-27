f42ee3ac818857b03adca7a5b13e0e0b33e61ace
mi82mi82 <daimi.0603@icolud.com>
2023-09-27 13:33:20 +0900

    ホーム画面の投稿の「 編集 」を押したときにindex.blade.phpの「.posts-edit」オブジェクトをeachで繰り返し処理する。 編集ボタンをクリックすると下の処理が行われるようにする。 dataメゾットとは、対象オブジェクトが持っているdata属性（data-name・data-targetなど）のみを取得します。
    var は変数のこと。
    
    getElementById とは、、、、get（取得する）Element（要素）ById（idを基準）＝　idを基準に要素を取得してる。
    
    むずい

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,21 @@ $(function() {
     $('.menu-list').slideToggle();
   });
 });
+
+// モーダル部分
+$(function () { //①
+  $('.posts-edit').each(function () {
+    $(this).on('click', function () {
+      console.log(1);
+      var target = $(this).data('target');
+      var modal = document.getElementById(target);
+      console.log(modal);
+      $(modal).fadeIn();
+      return false;
+    });
+  });
+  $('.modalClose').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  });
+});
