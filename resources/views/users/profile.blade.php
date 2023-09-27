52ee35890579a619f7c07156ce59faf940422208
mi82mi82 <daimi.0603@icolud.com>
2023-09-27 17:25:58 +0900

    プロフィール画面の表示。Auth:: でusersテーブルから情報持ってくる。
    名前はログインしているユーザー名が表示されるようにする。
    メールも同じく！
    パスワードは初期登録時に入力したパスワードを入れる。（表示はしないで⚫️で表示）
    
    newPasswordは、、、input typeをpasswordに変更。
    
    bioは、、、textarea nameがわかりにくい為bioに変更。
    　また初期登録時に入力したbioを表示する。

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -7,27 +7,25 @@
           <div class="ct-block">
             <img src="/storage/images/{{ $userimage }}">
             <label class="contact-text" for="name">UserName</label>
-            <input type="text" name="Username" placeholder="Dawntown" class="form-name">
+            <input type="text" name="Username" value="{{ Auth::user()->username }}" class="form-name">
           </div>
           <div class="ct-block">
             <label class="contact-text" for="email">MailAdress</label>
-            <input type="text" name="mail" placeholder="dawn@dawn.jp" class="form-mail">
+            <input type="text" name="mail" value="{{ Auth::user()->mail }}"class="form-mail">
           </div>
           <div class="ct-block">
           <label class="contact-text" for="email">Password</label>
-            <input type="text" name="Password" placeholder="" class="form-Password" value="readonly属性で入力規制" readonly>
+            <input type="password" name="Password"  class="form-Password" value="⚫︎⚫︎⚫︎⚫︎⚫︎⚫︎" readonly>
           </div>
-          <label class="contact-text" for="new Password">new Password</label>
-            <input type="text" name="new Password" placeholder="" class="form-new Password">
+          <label class="contact-text" for="newPassword">new Password</label>
+            <input type="password" name="newPassword"  class="form-new Password">
           </div>
           <div class="ct-block">
             <label for="request-about" class="contact-text">Bio</label>
-            <textarea name="request-about" id="request-about" cols="60" rows="6" placeholder="自己紹介文（任意）"></textarea>
+            <textarea name="bio" id="request-about" cols="60" rows="6">{{ Auth::user()->bio }}</textarea>
           </div>
           <div class="ct-block">
             <label for="request-about" class="contact-text">Iconimage</label>
-            <textarea name="request-about" id="request-about" cols="40" rows="4">
-            </textarea>
             <input id="image" type="file" name="image">
           </div>
           <div class="ct-block">
