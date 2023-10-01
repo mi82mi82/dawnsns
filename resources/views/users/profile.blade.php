bcf25a8ef64241398165fe4ff01891c395ef8355
mi82mi82 <daimi.0603@icolud.com>
2023-10-01 12:21:58 +0900

    ログインユーザーのプロフィールのバリデーションに引っかかったらエラーが出るようにする

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -33,4 +33,13 @@
           </div>
         </form>
 
+        @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 @endsection
